6eaf498a43b4179df9226f6b63bd5ebe8e592547
MAwais <mawais54013@outlook.com>
2019-02-26 21:43:05 -0800

    fixed design

--- main.js
+++ main.js
@@ -185,112 +185,114 @@ $('#online').on('click', 'button', (event) => {
 });
 
 // Form submit handler
-$('form').on('submit', (event) => {
-  event.preventDefault();
-  // Clear previous search results
-  if ($('#search').children().length > 0) {
-    $('#search').empty();
-  }
-  
-  // Show search suggestions if search field is empty and exit the function
-  if ($('input').val() === '') {
-    $('#search').append(`
-      <div class="col-12">
-        <p>Try searching for a game. Some suggestions are:</p>
-        <ul>
-          <li>World of Warcraft</li>
-          <li>Hearthstone</li>
-          <li>Starcraft 2</li>
-          <li>Heroes of the Storm</li>
-        </ul>
-      </div>
-    `);
-    return;
-  }
-  // AJAX request
-  const query = encodeURIComponent($('input').val());
-  $.getJSON(`https://api.twitch.tv/kraken/search/streams?query=${query}&client_id=vyuxu8oc2zuvwa0yau9e6jo2sq2li9`, (json) => {
-    if (json._total === 0) {
-      $('#search').append(`
-        <div class="col-12">
-          <p>Bummer! No results. Try searching for something else.</p>
-        </div>
-      `);
-    } else {
-      json.streams.forEach((item) => {
-        // Only append results that are not already in the online div
-        if ($(`#${item.channel.name}`).length === 0) {
-          $('#search').append(`
-            <div class="col-12">
-              <div class="row">
-                <div class="col-4 col-sm-2">
-                  <img src="${item.channel.logo || 'img/glitch.png'}">
-                </div>
-                <div class="col-8 col-sm-10">
-                  <h3><a href="${item.channel.url}" target="_blank">${item.channel.display_name}</a></h3>
-                  <p>${item.channel.status}</p>
-                  <p>Streaming ${item.game} for ${item.viewers} viewers</p>
-                  <button class="btn" value="${item.channel.name}" href="#${item.channel.name}">Click To Start</button>
-                </div>
-              </div>
-              <div class="collapse" id="${item.channel.name}" style="background: #000"></div>
-            </div>
-          `);
-        }
-      });
-    }
-  });
-  
-  // Clear input field
-  $('input').val('');
-});
 
-// Add delegated click handlers to search result buttons
-$('#search').on('click', 'button', (event) => {
-  const id = $(event.target).attr('href');
-  const name = $(event.target).val();
+
+// $('form').on('submit', (event) => {
+//   event.preventDefault();
+//   // Clear previous search results
+//   if ($('#search').children().length > 0) {
+//     $('#search').empty();
+//   }
   
-  // Live button
-  if ($(event.target).text() === 'Click To Start') {
-    // Close previously opened panel
-    if ($('.btn:contains("Hide")').length === 1) {
-      $('.btn:contains("Hide")').text('Click To Start');
-      $('.collapse:has("iframe")').collapse('hide').empty();
-    }
-    // Change button name to "Hide"
-    $(event.target).text('Hide');
-    // Append iframe into collapse div
-    $(id).append(`
-      <iframe
-        src="https://player.twitch.tv/?channel=${name}"
-        height="${((($('#search').width() - 30) * 9) / 16)}"
-        width="${($('#search').width() - 30)}"
-        frameborder="0"
-        scrolling="no"
-        allowfullscreen="true">
-      </iframe>
-    `);
-    // Make iframe responsive
-    $(window).on('resize', () => {
-      $('iframe').width($('#search').width() - 30);
-      $('iframe').height((($('#search').width() - 30) * 9) / 16);
-      $(id).width($('iframe').width());
-      $(id).height($('iframe').height());
-    });
-    // Start collapse animation
-    $(id).collapse('show');
-    // Fix container size after animation
-    $(id).on('webkitTransitionEnd oTransitionEnd transitionend', () => {
-      $(id).width($('iframe').width());
-      $(id).height($('iframe').height());
-    });
-  }
+//   // Show search suggestions if search field is empty and exit the function
+//   if ($('input').val() === '') {
+//     $('#search').append(`
+//       <div class="col-12">
+//         <p>Try searching for a game. Some suggestions are:</p>
+//         <ul>
+//           <li>World of Warcraft</li>
+//           <li>Hearthstone</li>
+//           <li>Starcraft 2</li>
+//           <li>Heroes of the Storm</li>
+//         </ul>
+//       </div>
+//     `);
+//     return;
+//   }
+//   // AJAX request
+//   const query = encodeURIComponent($('input').val());
+//   $.getJSON(`https://api.twitch.tv/kraken/search/streams?query=${query}&client_id=vyuxu8oc2zuvwa0yau9e6jo2sq2li9`, (json) => {
+//     if (json._total === 0) {
+//       $('#search').append(`
+//         <div class="col-12">
+//           <p>Bummer! No results. Try searching for something else.</p>
+//         </div>
+//       `);
+//     } else {
+//       json.streams.forEach((item) => {
+//         // Only append results that are not already in the online div
+//         if ($(`#${item.channel.name}`).length === 0) {
+//           $('#search').append(`
+//             <div class="col-12">
+//               <div class="row">
+//                 <div class="col-4 col-sm-2">
+//                   <img src="${item.channel.logo || 'img/glitch.png'}">
+//                 </div>
+//                 <div class="col-8 col-sm-10">
+//                   <h3><a href="${item.channel.url}" target="_blank">${item.channel.display_name}</a></h3>
+//                   <p>${item.channel.status}</p>
+//                   <p>Streaming ${item.game} for ${item.viewers} viewers</p>
+//                   <button class="btn" value="${item.channel.name}" href="#${item.channel.name}">Click To Start</button>
+//                 </div>
+//               </div>
+//               <div class="collapse" id="${item.channel.name}" style="background: #000"></div>
+//             </div>
+//           `);
+//         }
+//       });
+//     }
+//   });
   
-  // Hide button
-  // if ($(event.target).text() === 'Hide') {
-  //   if ($(id).hasClass('collapsing') === false) {
-  //     $(event.target).text('Click To Start');
-  //     $(id).collapse('hide').empty();
-  //   }
-  // }
-});
+//   // Clear input field
+//   $('input').val('');
+// });
+
+// // Add delegated click handlers to search result buttons
+// $('#search').on('click', 'button', (event) => {
+//   const id = $(event.target).attr('href');
+//   const name = $(event.target).val();
+  
+//   // Live button
+//   if ($(event.target).text() === 'Click To Start') {
+//     // Close previously opened panel
+//     if ($('.btn:contains("Hide")').length === 1) {
+//       $('.btn:contains("Hide")').text('Click To Start');
+//       $('.collapse:has("iframe")').collapse('hide').empty();
+//     }
+//     // Change button name to "Hide"
+//     $(event.target).text('Hide');
+//     // Append iframe into collapse div
+//     $(id).append(`
+//       <iframe
+//         src="https://player.twitch.tv/?channel=${name}"
+//         height="${((($('#search').width() - 30) * 9) / 16)}"
+//         width="${($('#search').width() - 30)}"
+//         frameborder="0"
+//         scrolling="no"
+//         allowfullscreen="true">
+//       </iframe>
+//     `);
+//     // Make iframe responsive
+//     $(window).on('resize', () => {
+//       $('iframe').width($('#search').width() - 30);
+//       $('iframe').height((($('#search').width() - 30) * 9) / 16);
+//       $(id).width($('iframe').width());
+//       $(id).height($('iframe').height());
+//     });
+//     // Start collapse animation
+//     $(id).collapse('show');
+//     // Fix container size after animation
+//     $(id).on('webkitTransitionEnd oTransitionEnd transitionend', () => {
+//       $(id).width($('iframe').width());
+//       $(id).height($('iframe').height());
+//     });
+//   }
+  
+//   // Hide button
+//   // if ($(event.target).text() === 'Hide') {
+//   //   if ($(id).hasClass('collapsing') === false) {
+//   //     $(event.target).text('Click To Start');
+//   //     $(id).collapse('hide').empty();
+//   //   }
+//   // }
+// });
